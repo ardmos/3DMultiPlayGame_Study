b82f9a1ed0f1709e3468a9f156e86d91869e76a7
ardmos <46891528+ardmos@users.noreply.github.com>
2021-11-06 16:39:33 +0900

    .

--- Assets/Script/TCPClientManager.cs
+++ Assets/Script/TCPClientManager.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// 클라이언트 매니저 입니다!
+/// 
+/// </summary>
+
 public class TCPClientManager : MonoBehaviour
 {
     // Start is called before the first frame update
