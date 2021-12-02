da20f358e64d6604f1ecddc303978d24fbd4d950
ardmos <46891528+ardmos@users.noreply.github.com>
2021-12-02 16:02:13 +0900

    애니메이션 문제 해결!!!  + base_mech 연속공격 가능하게 준비했음.
    블렌더에서 만든 애니메이션에 포지션값이 들어있었던것이 문제였다!

--- Assets/Script/base_mech/Base_mech_controller.cs
+++ Assets/Script/base_mech/Base_mech_controller.cs
@@ -4,61 +4,109 @@ using UnityEngine;
 
 public class Base_mech_controller : MonoBehaviour
 {
-    float move_speed;   //실제 이동 속도
-    float h_axis, v_axis;   //Input Axis 값을 받기 위한
-    Vector3 move_vec;   //위 두 개를 합쳐서 만든
+    public enum AttackState
+    {
+        Attack0,
+        Attack1,
+        Attack2,
+        Attack3
+    }
+    public AttackState next_attack_type;   //연속 공격을 위한 현재 공격 상태 체크 enum
+    float move_power;   //이동의 빠르기를 조절하는 변수
+    float h_axis, v_axis;   //Input Axis 값을 받기 위한 변수
+    Vector3 move_vec;   //위 두 개를 합쳐서 만든 변수
     Animator animator;
 
-    bool j_down, is_jump;
+    bool jump_down;
+    bool attack_down;
 
 
+
+    private void Awake()
+    {
+        move_power = 15f;
+        animator = GetComponentInChildren<Animator>();
+        next_attack_type = AttackState.Attack0;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        move_speed = 15f;
-        animator = GetComponent<Animator>();
+        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        h_axis = Input.GetAxisRaw("Horizontal");
-        v_axis = Input.GetAxisRaw("Vertical");
-        animator.SetFloat("Speed", Speed());    //달리기 애니메이션 재생을 위해 파라미터 전달.
-        move_vec = new Vector3(h_axis, 0, v_axis).normalized;   //대각선 이동시 속도가 빨라지는걸 막아주기 위해. 어떤 값이든 방향이 1로 보정되는 벡터. normalized를 사용.
-        transform.position += move_vec * move_speed * Time.deltaTime;    //실제 이동. transform 이동은 항상 Time.deltaTime까지. speed는 인스펙터상에서 정의.
-        transform.LookAt(transform.position + move_vec);    //나아가는 방향을 바라보기. 
-
-        if (Input.GetButtonDown("Jump")) animator.SetBool("Jump", true);
-        else if (Input.GetButtonUp("Jump")) animator.SetBool("Jump", false);
-
-        if (Input.GetKeyDown(KeyCode.Mouse0)) animator.SetTrigger("Attack");
-
+        GetInput();
+        Move();
+        Jump();
+        Attack();
     }
 
-    float Speed()
-    {
-        if (h_axis >= v_axis) return h_axis;
-        else return v_axis;
-    }
+
 
     void GetInput()
     {
-
+        h_axis = Input.GetAxisRaw("Horizontal");
+        v_axis = Input.GetAxisRaw("Vertical");
+        //Debug.Log(h_axis + ", " + v_axis);
     }
 
     void Move()
     {
-
+        animator.SetFloat("Speed", GetAxisValue());    //달리기 애니메이션 재생을 위해 파라미터 전달.
+        move_vec = new Vector3(h_axis, 0, v_axis).normalized;   //대각선 이동시 속도가 빨라지는걸 막아주기 위해. 어떤 값이든 방향이 1로 보정되는 벡터. normalized를 사용.
+        transform.position += move_vec * move_power * Time.deltaTime;    //실제 이동. transform 이동은 항상 Time.deltaTime까지. speed는 인스펙터상에서 정의.
+        transform.LookAt(transform.position + move_vec);    //나아가는 방향을 바라보기. 
     }
 
-    void Turn()
+    float GetAxisValue()   //h 이동 v 이동 모두 포함해서 이동중인지 확인하기 위한 메서드.
+    {
+        float return_axis = 0f;
+        if (h_axis != 0f) return_axis = h_axis;
+        if(v_axis != 0f) return_axis = v_axis;
+        return Mathf.Abs(return_axis);
+    }
+
+    void Jump()
     {
 
     }
 
-    void Jump()
+    void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            switch (next_attack_type)
+            {
+                case AttackState.Attack0:
+                    //콤보 시작
+                    animator.SetTrigger("Attack0");
+                    next_attack_type = AttackState.Attack1;
+                    break;
+                case AttackState.Attack1:
+                    animator.SetTrigger("Attack0");
+                    next_attack_type = AttackState.Attack2;
+                    break;
+                case AttackState.Attack2:
+                    animator.SetTrigger("Attack0");
+                    next_attack_type = AttackState.Attack3;
+                    break;
+                case AttackState.Attack3:
+                    animator.SetTrigger("Attack0");
+                    next_attack_type = AttackState.Attack0;
+                    //끝
+                    break;
+                default:
+                    Debug.Log("공격상태가 이상합니다. ${attack_state}");
+                    break;
+            }
+        }
+    }
+
+    void LookAround()
     {
 
     }
