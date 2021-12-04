a331338bcaa60154161d3f276e50a79a9129eb65
ardmos <46891528+ardmos@users.noreply.github.com>
2021-12-04 11:28:37 +0900

    character controller를 사용한 캐릭터 이동과 회전, cinemachine freelook 사용해서 화면회전까지 구현했는데 화면회전이 마우스 방향 반대로 된다.  이 문제 해결중!

---
+++ Assets/Script/ThirdPersonMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public Transform cam;
+    public float speed = 12f;
+    public float turn_smooth_time = 0.1f;
+    float turn_smoot_velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float target_angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target_angle, ref turn_smoot_velocity, turn_smooth_time);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+
+            Vector3 moveDir = Quaternion.Euler(0f, target_angle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+
+    void GetInput()
+    {
+
+    }
+
+    void Move()
+    {
+
+    }
+}
