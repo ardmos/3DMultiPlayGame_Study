89b0aae975fa91bf4b7b38d241661c7776f1ace8
ardmos <46891528+ardmos@users.noreply.github.com>
2021-12-04 12:03:40 +0900

    중력과 점프까지 구현 완료!

--- Assets/Script/ThirdPersonMovement.cs
+++ Assets/Script/ThirdPersonMovement.cs
@@ -6,37 +6,76 @@ public class ThirdPersonMovement : MonoBehaviour
 {
 
     public CharacterController controller;
-    public Transform cam;
+    public Transform cam;   
+    float horizontal;
+    float vertical;    
     public float speed = 12f;
-    public float turn_smooth_time = 0.1f;
-    float turn_smoot_velocity;
+    public float turn_smooth_time = 0.1f;    
+    float turn_smoot_velocity;    
+    Vector3 direction;
+
+    public float gravity = -9.81f;
+    Vector3 gravity_velocity;
+
+    public Transform ground_check;
+    public float ground_distance = 0.4f;
+    public LayerMask ground_mask;
+    bool is_grounded;
+
+    public float jump_height = 3f;
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        IsGrounded();
+        GetInput();
+        Move();
+        Jump();        
+        Gravity();
+    }
 
-        if(direction.magnitude >= 0.1f)
+
+    void IsGrounded()
+    {
+        is_grounded = Physics.CheckSphere(ground_check.position, ground_distance, ground_mask);
+        if (is_grounded && gravity_velocity.y < 0)
+        {
+            gravity_velocity.y = -2f;
+        }
+    }
+
+    void GetInput()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void Move()
+    {
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
         {
             float target_angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target_angle, ref turn_smoot_velocity, turn_smooth_time);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
 
             Vector3 moveDir = Quaternion.Euler(0f, target_angle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
     }
 
-    void GetInput()
+    void Jump()
     {
-
+        if (Input.GetButtonDown("Jump") && is_grounded)
+        {
+            gravity_velocity.y = Mathf.Sqrt(jump_height * -2f * gravity);
+        }
     }
 
-    void Move()
+    void Gravity()
     {
-
+        gravity_velocity.y += gravity * Time.deltaTime;
+        controller.Move(gravity_velocity * Time.deltaTime);
     }
 }
